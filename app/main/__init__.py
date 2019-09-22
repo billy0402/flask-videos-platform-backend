55532b66c5a6a1932b7066ce14259f9ae400ed06
Billy Huang <10646003@ntub.edu.tw>
2019-09-23 00:37:57 +0800

    flask - add flask-cors

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -1,5 +1,11 @@
+import re
+
 from flask import Blueprint
+from flask_cors import CORS
 
 main = Blueprint('main', __name__)
+CORS(main, resources={r'/api/*': {'origins': re.compile(
+    r'^https?://(?:localhost:(?:3000|5000)|videos-platform-backend.herokuapp.com)$'
+)}})
 
 from . import views, errors
